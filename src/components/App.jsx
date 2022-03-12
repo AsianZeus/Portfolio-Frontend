90785b83352407bcbfff0eb5b337bbc6055c5974
Akshat Surolia <ace@Akshats-MacBook-Air.local>
2022-03-12 15:47:06 +0530

    testing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,11 @@ import { QueryClientProvider, QueryClient } from 'react-query'
 import Main from "./Main";
 function App() {
   const queryClient = new QueryClient()
-
   return (
       <div className="App">
         <QueryClientProvider client={queryClient}>
           <Main />
+          {console.log(process.env.SECRET_KEY)}
           </QueryClientProvider>
       </div>
   );
